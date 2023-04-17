12b225cf6e5a7f73225039b8ea9e23c5c95dacca
JONGMIN KIM <bellmin9321@gmail.com>
2023-04-18 00:32:51 +0900

    ✅ Add: prettier 추가

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'auto',
+};
