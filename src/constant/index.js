750cf1d9d64ec79cd8dd571d5fcf6e419a1e779d
JONGMIN KIM <bellmin9321@gmail.com>
2023-04-19 02:24:38 +0900

    ✅ Add: constant폴더에서 변수 관리

---
+++ src/constant/index.js
@@ -0,0 +1,2 @@
+export let description =
+  'nextjs와 json-server를 이용한 프로젝트입니다. react-bootstrap으로 css 디자인을 했고, recoil을 이용하여 json의 data들을 전역 상태로 관리했습니다. 각 STORE를 클릭 시 모달 팝업이 생기며 맛집 이름, 사진, 설명, URL을 표시했습니다. About/Sore/NotFound 페이지별 서비스 로직은 커스텀훅으로 만들어 view와 따로 분리해서 관리했습니다.';
