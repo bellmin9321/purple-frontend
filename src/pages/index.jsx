55d2d703ad278e18a4c10f06273e6a212110bb98
JONGMIN KIM <bellmin9321@gmail.com>
2023-04-19 01:14:04 +0900

    ✨ Feat: Home page 구현

---
+++ src/pages/index.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <>
+      <h1 style={{ fontSize: '100px', color: 'green', marginBottom: 30 }}>
+        Homepage
+      </h1>
+
+      <h2>
+        <Link href="/about">ABOUT</Link>과 <Link href="/store">STORE</Link>
+        메뉴를 눌러서 이용해보세요
+      </h2>
+    </>
+  );
+}
