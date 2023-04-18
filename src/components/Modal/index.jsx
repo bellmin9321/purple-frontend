850af2806882f7232abdf72499c547bf6351ef4b
JONGMIN KIM <bellmin9321@gmail.com>
2023-04-19 01:16:14 +0900

    ✨ Feat: store 클릭 시 모달 구현

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,56 @@
+import Link from 'next/link';
+import Modal from 'react-bootstrap/Modal';
+import { useRecoilValue } from 'recoil';
+
+import useStore from '@/hooks/useStore';
+
+import { selectedStore } from '@/recoil';
+
+function StoreModal() {
+  const { show, setShow } = useStore();
+  const store = useRecoilValue(selectedStore);
+
+  const insertEnter = (str) => {
+    const arr = str.split('.');
+    return arr.join('.<br /><br />');
+  };
+
+  return (
+    <Modal
+      size="xl"
+      show={show}
+      onHide={() => setShow(false)}
+      aria-labelledby="custom-modal"
+      centered
+    >
+      <Modal.Body
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+        }}
+      >
+        <img
+          src={store.image}
+          alt="store"
+          style={{
+            width: '25rem',
+            height: '30rem',
+            marginRight: 20,
+          }}
+        />
+        <section>
+          <Modal.Header closeButton style={{ border: 'none', fontSize: 25 }} />
+          <Modal.Title id="custom-modal" style={{ marginBottom: 10 }}>
+            {store.name}
+          </Modal.Title>
+          <p
+            dangerouslySetInnerHTML={{ __html: insertEnter(store.description) }}
+          />
+          {store.url && <Link href={store.url}>{store.url}</Link>}
+        </section>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+export default StoreModal;
