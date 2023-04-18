7dbf595231c0e8255e0c8d1762ff845082663895
JONGMIN KIM <bellmin9321@gmail.com>
2023-04-18 22:16:25 +0900

    ✅ Add: Footer 컴포넌트 추가

---
+++ src/components/layout/Footer/index.jsx
@@ -0,0 +1,20 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: 'black',
+        color: 'white',
+        height: '7vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div>AWESOME FOOD STORE</div>
+      <div>@ 2020 김종민</div>
+    </footer>
+  );
+}
+
+export default Footer;
